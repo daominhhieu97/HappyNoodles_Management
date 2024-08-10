11a57c763b72e14dfa628a7193939ab87dbc858d
Dao Minh Hieu <hieudm97@gmail.com>
2024-08-10 18:47:16 +0700

    HN-21 add a form to add a category and link it to a menu.

--- Services/ICategoryService.cs
+++ Services/ICategoryService.cs
@@ -1,10 +1,12 @@
 using HappyNoodles_ManagementApp.Models;
+using HappyNoodles_ManagementApp.ViewModels.Category;
 using HappyNoodles_ManagementWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 
 public interface ICategoryService
 {
     Task<List<Category>> GetCategories();
+    Task AddCategoryAsync(CategoryViewModel model);
     //Task<Category> GetCategory(int id);
     //Task AddCategory(Category category);
     //Task UpdateCategory(Category category);
@@ -22,7 +24,21 @@ public class CategoryService : ICategoryService
 
     public async Task<List<Category>> GetCategories()
     {
-        return await _context.Categories.Include(x => x.Menu).AsNoTracking().ToListAsync();
+        return await _context.Categories
+            .Include(x => x.Menu)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+    public async Task AddCategoryAsync(CategoryViewModel model)
+    {
+        var category = new Category
+        {
+            Name = model.Name,
+            MenuId = model.Menu.Id
+        };
+
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
     }
 
     //public async Task<Category> GetCategory(int id)
